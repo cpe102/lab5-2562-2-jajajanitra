c7436a94c559a50ea9397202a3d4527a5090d6a1
lab302-6 <lab302-6@labeng.local>
2020-01-07 16:56:01 +0700

    first

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
@@ -6,17 +7,30 @@ int main(){
 	cout << "Enter the number of student: ";
 	cin >> N;
 	
+	int A =0;
 	string name[N];
 	int age[N];
+	while(A < N){
 	cout << "Name of student = ";
+	cin >> name[A];
 	cout << "Age of student = ";
-	
+	cin >> age[A];
+	A++;
+	}
+
 	int key;
 	cout << "--------------------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
 	cout << "--------------------------------------\n";
 	
+	int i=0;
+	while(i<N){
+		if(key == age[i]){
+			cout << name[i] << "\n";
+		}
+		i++;
+	}
 
 	
 	cout << "--------------------------------------\n";
